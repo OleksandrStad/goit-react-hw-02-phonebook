aaf3e057bbc10b23e9783c3d052f39a764b6352e
OleksandrStad <mmk.polymer@gmail.com>
2023-03-24 19:41:24 +0200

    add number

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -5,7 +5,7 @@ import { Btn, Form } from "./Form.styled"
 export class ContactForm extends Component {
     state = {
         name: '',
-
+        number: '',
     };
 
     handleChange = e => {
@@ -18,14 +18,14 @@ export class ContactForm extends Component {
         const contact = {
             id: nanoid(),
             name: this.state.name,
-
+            number: this.state.number,
         }
 
         this.props.onSubmit(contact)
 
         this.setState({
             name: '',
-
+            number: '',
         })
     };
 
@@ -45,9 +45,19 @@ export class ContactForm extends Component {
                             onChange={this.handleChange}
                             value={this.state.name}
                         />
-
                     </label>
-
+                    <label htmlFor="">
+                        <p>Number</p>
+                        <input
+                            type="tel"
+                            name="number"
+                            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                            required
+                            onChange={this.handleChange}
+                            value={this.state.number}
+                        />
+                    </label>
                     <Btn type="submit" >add contact</Btn >
                 </Form>
             </div >
